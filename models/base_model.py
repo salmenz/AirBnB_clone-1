d69eee677fde54c40812cdafd175d5f14a8c8b78
taiebchaabini <taiebchaabini@gmail.com>
2020-02-17 10:37:34 +0000

    Implementing *args and *kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+
+
 class BaseModel():
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         self.id = str(uuid.uuid4())
-    
+        if kwargs is None:
+            self.created_at = datetime.now()
+            self.id = str(uuid.uuid4())
+        else:
+            for k, v in kwargs.items():
+                if "__class__" != k:
+                    if k in ("created_at", "updated_at"):
+                        setattr(self, k, datetime.strptime(v,
+                                "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, k, v)
+
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
-                self.__dict__)
+                                     self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
@@ -24,7 +37,7 @@ class BaseModel():
                 name = i.split("__")[0]
             if name in ("updated_at", "created_at"):
                 my_dict[name] = datetime.strftime(self.__dict__[i],
-                        "%Y-%m-%dT%H:%M:%S.%f")
+                                                  "%Y-%m-%dT%H:%M:%S.%f")
             else:
                 my_dict[name] = self.__dict__[i]
         my_dict["__class__"] = self.__class__.__name__
