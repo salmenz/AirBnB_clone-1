2b9bffbec7b55320cb3a026f6ee38ca1c41b586d
taiebchaabini <taiebchaabini@gmail.com>
2020-02-17 15:06:29 +0000

    New changes

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+from . import storage
 
-
+""" BaseModel that defines all common attributes/methods for other classes."""
 class BaseModel():
+    """ BaseModel that defines all
+    common attributes/methods for other classes."""
     def __init__(self, *args, **kwargs):
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -11,6 +14,7 @@ class BaseModel():
         if kwargs is None:
             self.created_at = datetime.now()
             self.id = str(uuid.uuid4())
+            storage.new()
         else:
             for k, v in kwargs.items():
                 if "__class__" != k:
@@ -21,13 +25,22 @@ class BaseModel():
                         setattr(self, k, v)
 
     def __str__(self):
+        """ should print: [<class name>] (<self.id>) <self.__dict__> """
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
                                      self.__dict__)
 
     def save(self):
+        """ updates the public instance attribute updated_at with the current
+        datetime
+        """
         self.updated_at = datetime.now()
+        storage.new(self)
+        storage.save()
 
     def to_dict(self):
+        """ returns a dictionary containing all keys/values of __dict__ of
+        the instance:
+        """
         my_dict = {}
         for i in self.__dict__.keys():
             name = i.split("__")
