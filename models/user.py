47e90bf37c0d8e91661e87b526d35dd5077540a0
taiebchaabini <taiebchaabini@gmail.com>
2020-02-19 21:16:12 +0000

    Adding new class : User

---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+from models import BaseModel
+""" class User that inherits from BaseModel
+"""
+class User(BaseModel):
+    """ class User that inherits from BaseModel
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
