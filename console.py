dc2f776d9792ca17071378c71ac0ac3c622588c8
taiebchaabini <taiebchaabini@gmail.com>
2020-02-20 11:04:05 +0000

    Implementing : Place, State, City, Amenity, Review

--- console.py
+++ console.py
@@ -7,6 +7,11 @@ import inspect
 from models.base_model import BaseModel
 from models import storage
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 """ program called console.py that contains the entry point of the command
 interpreter """
 
